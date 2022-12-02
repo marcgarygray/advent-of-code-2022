4be47fb1918e8f918eba50418a859028e1a147a7
Marc Gray <marc.gray@dutchie.com>
2022-12-02 13:38:22 -0800

    day one part two

--- src/day-one/solution.ts
+++ src/day-one/solution.ts
@@ -1,31 +1,36 @@
-import fs from 'fs';
-import path from 'path';
-import events from 'events';
-import readline from 'readline';
+import fs from "fs";
+import path from "path";
+import events from "events";
+import readline from "readline";
 
-export async function getDayOneAnswer(): Promise<number> {
+type TopThree = {
+  1: number;
+  2: number;
+  3: number;
+};
+
+export async function getTopThreeElves(): Promise<TopThree> {
   const lineReader = readline.createInterface({
     input: fs.createReadStream(
-      path.join(__dirname, '..', '..', 'src', 'day-one', 'inputs.txt'),
-      'utf-8'
+      path.join(__dirname, "..", "..", "src", "day-one", "inputs.txt"),
+      "utf-8"
     ),
   });
 
   let workingSum = 0;
-  let max = 0;
-
-  lineReader.on('line', (line) => {
-    if (line !== '') {
+  const elves: number[] = [];
+  lineReader.on("line", (line) => {
+    if (line !== "") {
       workingSum += Number(line);
     } else {
-      if (workingSum > max) {
-        max = workingSum;
-      }
+      elves.push(workingSum);
       workingSum = 0;
     }
   });
 
-  await events.once(lineReader, 'close');
+  await events.once(lineReader, "close");
 
-  return max;
+  elves.sort((a, b) => b - a);
+
+  return { 1: elves[0], 2: elves[1], 3: elves[2] };
 }
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 #!/usr/bin/env node
-import chalk from 'chalk';
-import { getDayOneAnswer } from './day-one/solution';
+import chalk from "chalk";
+import { getTopThreeElves } from "./day-one/solution";
 
 (async () => {
-  const dayOneAnswer = await getDayOneAnswer();
+  const topThreeElves = await getTopThreeElves();
   console.log(`
-    ${chalk.magenta('Day One Answer:')}
-    ${chalk.bgMagenta.white(`       `)}
-    ${chalk.bgMagenta.white(` ${dayOneAnswer} `)}
-    ${chalk.bgMagenta.white(`       `)}
+    ${chalk.magenta("Day One Answers:")}
+    ${chalk.magenta(` Top Elf:`)}
+     ${chalk.bgMagenta.white(`        `)}
+     ${chalk.bgMagenta.white(` ${topThreeElves[1]}  `)}
+     ${chalk.bgMagenta.white(`        `)}
+    ${chalk.magenta(` Top Three Elves:`)}
+     ${chalk.bgMagenta.white(`        `)}
+     ${chalk.bgMagenta.white(
+       ` ${topThreeElves[1] + topThreeElves[2] + topThreeElves[3]} `
+     )}
+     ${chalk.bgMagenta.white(`        `)}
   `);
 })();
