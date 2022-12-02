379a0a2cf66aa43dcc13749b8d1b821969d98825
Marc Gray <marcgarygray@gmail.com>
2022-12-01 21:58:09 -0800

    scaffold the app, day one solution (#1)
    Co-authored-by: marcgray <marc@marcgarygray.com>

---
+++ dist/src/index.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const solution_1 = require("../day-one/solution");
+(0, solution_1.getData)();
+console.log('hello moto');
