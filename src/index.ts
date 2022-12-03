e2d539148d4b2982bb26c8fee1e36f26e7cd94e3
marcgray <marc@marcgarygray.com>
2022-12-02 21:42:48 -0800

    day two part two

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import chalk from 'chalk';
 import { getTopThreeElves } from './day-one/solution';
-import { getTotalScore } from './day-two/solution';
+import { getTotalScore, getTotalScorePartTwo } from './day-two/solution';
 
 (async () => {
   const topThreeElves = await getTopThreeElves();
@@ -22,11 +22,16 @@ import { getTotalScore } from './day-two/solution';
 
 (async () => {
   const totalScore = await getTotalScore();
+  const totalScorePartTwo = await getTotalScorePartTwo();
   console.log(`
     ${chalk.magenta('Day Two Answers:')}
     ${chalk.magenta(` Total Score:`)}
      ${chalk.bgMagenta.white(`        `)}
      ${chalk.bgMagenta.white(` ${totalScore}  `)}
+     ${chalk.bgMagenta.white(`        `)}
+    ${chalk.magenta(` Total Score After Getting More Elf Intelligence:`)}
+     ${chalk.bgMagenta.white(`        `)}
+     ${chalk.bgMagenta.white(` ${totalScorePartTwo}  `)}
      ${chalk.bgMagenta.white(`        `)}
   `);
 })();
